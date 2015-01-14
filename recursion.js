93ae0fa304b99225a31233dd89d1e29dd271c0db
Stephen Done <srdone@gmail.com>
2015-01-14 06:45:52 -0700

    non-functioning: adds framework for recursive isEven, but only loose pseudocode so far

---
+++ recursion.js
@@ -0,0 +1,14 @@
+// Returns true when a number is even, false when a number is odd
+// Uses recursion based on the following rules:
+// Zero is even
+// One is odd
+// For any number N, it's evenness is the same as N-2
+//
+// To handle negative numbers, converts them to positive numbers.
+
+function isEven(x) {
+  var num = Math.abs(x); //handle negative numbers
+
+  // Check to see if the value is zero or one and returns corresponing response
+  // Otherwise performs a recursion after subtracting two.
+}
