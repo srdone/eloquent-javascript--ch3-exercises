c4643cca9733012a7d3e92e3c87be41e625f8b2a
Stephen Done <srdone@gmail.com>
2015-01-14 18:02:04 -0700

    Wrote countChar

---
+++ beanCounting.js
@@ -0,0 +1,17 @@
+// countChar returns the number of characters in the given string that match the given char
+
+function countChar(string, chr) {
+
+  var totalCount = 0;
+
+  for (var i = 0; i < string.length; i++) {
+    if (string.charAt(i) === chr) {
+      totalCount++;
+    }
+  }
+
+  return totalCount;
+
+}
+
+console.log(countChar('Alpha A', 'A'));
